84087b562a285b5ca8d48739e7fb5f2d80c353d3
daivik05 <4nm21is038@nmamit.in>
2026-05-06 21:09:15 +0530

    commit

--- network.py
+++ network.py
@@ -6,6 +6,8 @@ def create_network():
     # VMs
     vm1 = VM("VM1", "10.0.0.2")
     vm2 = VM("VM2", "40.0.0.2")
+    vm3 = VM("VM3", "20.0.0.2")
+    vm4 = VM("VM4", "30.0.0.2")
 
     # Routers
     r1 = Router("R1")
@@ -13,7 +15,7 @@ def create_network():
     r3 = Router("R3")
     r4 = Router("R4")
 
-    # Connect routers (BFS graph - no weights)
+    # Router connections
     r1.connect_router(r2)
     r2.connect_router(r1)
 
@@ -26,11 +28,16 @@ def create_network():
     r1.connect_router(r4)
     r4.connect_router(r1)
 
-    # Connect VMs
+    # Connect VMs to routers
     vm1.connect_router(r1)
     vm2.connect_router(r4)
+    vm3.connect_router(r2)
+    vm4.connect_router(r3)
 
+    # Assign interfaces (network mapping)
     r1.add_interface("10.0.0.1", vm1)
+    r2.add_interface("20.0.0.1", vm3)
+    r3.add_interface("30.0.0.1", vm4)
     r4.add_interface("40.0.0.1", vm2)
 
     return vm1, vm2, r1
